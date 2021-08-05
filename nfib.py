22daf5f9255abc293f292ded0a13f8c6af8e9951
aishwarya6018 <52097087+aishwarya6018@users.noreply.github.com>
2021-08-05 12:11:15 +0530

    Update nfib.py

--- nfib.py
+++ nfib.py
@@ -1,24 +1,17 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
-
 #Generate nth Fibonacci number
 
-memo={}
+memo={} #Create a dictionary to store the values generated.
 def fib(n):
-    if n in memo:
+    if n in memo: #Check if n is in the dictionary.
         return memo[n]
     elif n<=2:
         return 1
     else:
-        memo[n]=fib(n-1)+fib(n-2)
-    return memo[n]
-n=int(input())
-result=fib(n)
-print("The {}th fibonacci number is : ".format(n),result)
+        memo[n]=fib(n-1)+fib(n-2) #Store the newly generated value in the dictionary, so that it will be useful in later computations.
+    return memo[n] #Return the value of the nth fibonacci number.
+  
+n=int(input()) #Take input from the user.
+result=fib(n) #Pass the input to the function and store the return value (Which is the answer) in a variable.
+print("The {}th fibonacci number is : ".format(n),result) #Print the result i.e., the nth fibonacci number.
 
 
